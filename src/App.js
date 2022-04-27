b3b062fa1aaec14d085901205de38b217dab92ad
HemantTrellance <hkhinchi.trellance@gmail.com>
2022-04-27 12:59:28 +0530

    1 file modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React from "react";
-import "./style.css";
-
+import React from 'react';
+import './style.css';
+import Users from './component/Users';
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Users />
     </div>
   );
 }
